d0efa11a49a2b85c8534c734496afb64621be27f
David Roy <david.roy@bimone.com>
2018-08-31 15:36:30 -0400

    Created a folder for each versions supported.

---
+++ 2019/GroupClashes/GroupClashes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace GroupClashes
+{
+    [Plugin("GroupClashes", "BIMO", DisplayName = "Group Clashes")]
+    //[Strings("GroupClashes.name")]
+    //[RibbonLayout("GroupClashes.xaml")]
+    [RibbonTab("ID_GroupClashesTab", DisplayName = "Group Clashes")]
+    [Command("ID_GroupClashesButton",
+             Icon = "GroupClashesIcon_Small.ico", LargeIcon = "GroupClashesIcon_Large.ico",
+             DisplayName = "Group Clashes", CanToggle = false)]
+
+    class RibbonHandler : CommandHandlerPlugin
+    {
+        public override int ExecuteCommand(string commandId, params string[] parameters)
+        {
+            if (Autodesk.Navisworks.Api.Application.IsAutomated)
+            {
+                throw new InvalidOperationException("Invalid when running using Automation");
+            }
+
+            //Find the plugin
+            PluginRecord pr = Autodesk.Navisworks.Api.Application.Plugins.FindPlugin("GroupClashes.GroupClashesPane.BIMO");
+
+            if (pr != null && pr is DockPanePluginRecord && pr.IsEnabled)
+            {
+                //check if it needs loading
+                if (pr.LoadedPlugin == null)
+                {
+                    pr.LoadPlugin();
+                }
+
+                DockPanePlugin dpp = pr.LoadedPlugin as DockPanePlugin;
+                if (dpp != null)
+                {
+                    //switch the Visible flag
+                    dpp.Visible = !dpp.Visible;
+                }
+            }
+
+            return 0;
+        }
+
+        public override CommandState CanExecuteCommand(String commandId)
+        {
+            CommandState state = new CommandState();
+            state.IsVisible = true;
+            state.IsEnabled = true;
+            state.IsChecked = true;
+
+            return state;
+        }
+
+        public override bool CanExecuteRibbonTab(string name)
+        {
+            return false;
+        }
+
+        public override bool TryShowCommandHelp(string name)
+        {
+            //FileInfo dllFileInfo = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            //string pathToHtmlFile = Path.Combine(dllFileInfo.Directory.FullName, @"Help\Help.html");
+            //System.Diagnostics.Process.Start(pathToHtmlFile);
+            //return true;
+            return false;
+        }
+    }
+}
+
+
