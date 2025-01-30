28f49ca863215e05d56fd58d51f3c2327018e84c
IVAN LARA <ivan.lara-rodriguez@bimone.com>
2025-01-30 15:02:15 -0500

    add 2025 support

---
+++ 2025/GroupClashes/GroupClashesPane.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace BimOne.BimTrack.GroupClashes
+{
+    [Plugin("BimOne.BimTrack.GroupClashes.GroupClashesPane", "BIMO", DisplayName = "Newforma KONEKT Group Clashes",ToolTip = "Newforma KONEKT Group clashes")]
+    [DockPanePlugin(300, 380)]
+    class GroupClashesPane : DockPanePlugin
+    {
+        public override Control CreateControlPane()
+        {
+            //create the control that will be used to display in the pane
+            GroupClashesHostingControl control = new GroupClashesHostingControl();
+
+            control.Dock = DockStyle.Fill;
+
+            //create the control
+            control.CreateControl();
+
+            return control;
+        }
+
+        public override void DestroyControlPane(Control pane)
+        {
+            pane.Dispose();
+        }
+    }
+}
